5db964840d14f7ff2a9bcfa8b9545fd3bb54d401
Andult <104719742+AJY3124@users.noreply.github.com>
2024-04-22 17:39:52 +0900

    docs : 코드 중간중간의 여백을 줄여봤습니다.

--- monty.py
+++ monty.py
@@ -3,26 +3,18 @@ from random import randint
 k = int(input('monti time : '))
 
 def montihol(time) :
-
     suc = 0 #자동차를 고른 횟수
     fail = 0 #염소를 고른 횟수
-
     for i in range(time) :
         car = randint(0, 2)
-        
         doors = [0, 0, 0]
-
         doors[car] +=1
-
         choice = randint(0, 2)
-
         if doors[choice] == 0 :
             doors[choice] +=2
         else:
             doors[choice] = 3
-
         doors.remove(0)
-
         if 2 in doors:
             suc+=1
         else :
